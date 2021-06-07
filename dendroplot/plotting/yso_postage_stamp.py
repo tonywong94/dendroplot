cdf02a96bda58e0d92c840b3c6ec38cac1d7a3c2
Alex Green <72169529+grnalx@users.noreply.github.com>
2021-06-07 09:25:54 -0500

    Add files via upload

--- dendroplot/plotting/yso_postage_stamp.py
+++ dendroplot/plotting/yso_postage_stamp.py
@@ -1,12 +1,44 @@
-import numpy as np
-from spectral_cube import SpectralCube
-from astropy import units as u
 from matplotlib import pyplot as plt
 from astropy.io.fits import getheader, getdata
 from astropy.wcs import WCS
 from astropy.coordinates import SkyCoord as SkyCoord
+from spectral_cube import SpectralCube
 import astropy
+import pickle
+import astropy.units as u
+from astropy.wcs.utils import skycoord_to_pixel
+import numpy as np
 
+def yso_asgn_structures(cube_location, YSO_RA, YSO_DEC,name):
+    cube,header = (getdata(cube_location,header=True))
+    wcs = WCS(header)
+    pixel_ra,pixel_dec = skycoord_to_pixel(SkyCoord(YSO_RA,YSO_DEC), wcs)
+    pixel_ra = pixel_ra.data.tolist()
+    pixel_dec = pixel_dec.data.tolist()
+    p1 = [cube[i][int(np.floor(pixel_dec))][int(np.floor(pixel_ra))] for i in range(len(cube))]
+    p2 = [cube[i][int(np.ceil(pixel_dec))][int(np.ceil(pixel_ra))] for i in range(len(cube))]
+    p3 = [cube[i][int(np.ceil(pixel_dec))][int(np.floor(pixel_ra))] for i in range(len(cube))]
+    p4 = [cube[i][int(np.floor(pixel_dec))][int(np.ceil(pixel_ra))] for i in range(len(cube))]
+    p1_d = dict(zip(np.arange(len(p1)),p1))
+    p2_d = dict(zip(np.arange(len(p2)),p2))
+    p3_d = dict(zip(np.arange(len(p3)),p3))
+    p4_d = dict(zip(np.arange(len(p4)),p4))
+    dicts = p1_d,p2_d,p3_d,p4_d 
+    with open(name+".txt", "wb") as f:
+        pickle.dump(dicts, f)
+    return
+def get_yso_asgn(name):
+    '''returns tuple of dictionaries of the non-nan values of asgn cube
+    
+    '''
+    with open(name+".txt","rb") as f:
+        out = pickle.load(f)
+    p1 = {i:int(out[0][i]) for i in range(len(out[0])) if ~np.isnan(out[0][i])}
+    p2 = {i:int(out[1][i]) for i in range(len(out[1])) if ~np.isnan(out[1][i])}
+    p3 = {i:int(out[2][i]) for i in range(len(out[2])) if ~np.isnan(out[2][i])}
+    p4 = {i:int(out[3][i]) for i in range(len(out[3])) if ~np.isnan(out[3][i])}
+    return p1,p2,p3,p4
+    
 def yso_postage_stamp(cube_location, YSO_RA, YSO_DEC, RA_width=1*u.arcmin, DEC_width=1*u.arcmin,slice_=None):
     cube,header = (getdata(cube_location,header=True))
     wcs = WCS(header)
