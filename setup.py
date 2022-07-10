8980ef7759b8b82c6a0e962fb9884134c05dd9b4
Tony Wong <wongt@illinois.edu>
2022-07-10 12:18:20 -0700

    Update version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='dendroplot',
-    version='0.3.2',
+    version='0.5.0',
     description='Helper tasks for astrodendro',
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -18,7 +18,7 @@ setup(name='dendroplot',
                         'kapteyn',
                         'astrodendro'],
     url='https://github.com/tonywong94/dendroplot',
-    download_url = 'https://github.com/tonywong94/dendroplot/archive/refs/tags/v0.3.2.tar.gz', 
+    download_url = 'https://github.com/tonywong94/dendroplot/archive/refs/tags/v0.5.0.tar.gz', 
     packages=['dendroplot', 'dendroplot.analysis', 'dendroplot.lte', 
               'dendroplot.plotting'],
     )
